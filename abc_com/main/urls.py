bd95519944990bf0f832bdfb7d7e36e799785c5d
drexqq <01037923530a@gmail.com>
2021-03-19 00:50:34 +0900

    blog page test

--- abc_com/main/urls.py
+++ abc_com/main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='main'
+
 urlpatterns = [
-    path('', views.index, name='index'),        
+    path('', views.index, name='index'),
+    path('blog/', views.blog, name='blog'),
 ]
